a4c0aea4778c46a228fefb7e89e0aaacd5bb27e9
andreaTP <andrea.peruffo1982@gmail.com>
2022-03-24 16:39:49 +0000

    more improvements

--- operator/scripts/inject-custom-image.sh
+++ operator/scripts/inject-custom-image.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 VERSION="$1"
-JAVA_OPTS="$2"
+VALUE="$2"
 
-yq ea -i ".spec.install.spec.deployments[0].spec.template.spec.containers[0].env += {\"name\": \"JAVA_OPTS\", \"value\": \"$JAVA_OPTS\"}" $SCRIPT_DIR/../olm/$VERSION/manifests/keycloak-operator.v$VERSION.clusterserviceversion.yaml
+yq ea -i ".spec.install.spec.deployments[0].spec.template.spec.containers[0].env += {\"name\": \"OPERATOR_KEYCLOAK_IMAGE\", \"value\": \"$VALUE\"}" $SCRIPT_DIR/../olm/$VERSION/manifests/keycloak-operator.v$VERSION.clusterserviceversion.yaml
