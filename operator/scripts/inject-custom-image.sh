9f7ebcc377ffbe3fd3d474eb7cf2e0599a16997c
andreaTP <andrea.peruffo1982@gmail.com>
2022-03-24 15:59:35 +0000

    more

--- operator/scripts/inject-custom-image.sh
+++ operator/scripts/inject-custom-image.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VERSION="$1"
 JAVA_OPTS="$2"
 
-yq ea -i ".spec.install.spec.deployments[0].spec.template.spec.containers[0].env += {\"name\": \"JAVA_OPTS\", \"value\": \"$JAVA_OPTS\"}" olm/0.0.2/manifests/keycloak-operator.v0.0.2.clusterserviceversion.yaml
+yq ea -i ".spec.install.spec.deployments[0].spec.template.spec.containers[0].env += {\"name\": \"JAVA_OPTS\", \"value\": \"$JAVA_OPTS\"}" $SCRIPT_DIR/../olm/$VERSION/manifests/keycloak-operator.v$VERSION.clusterserviceversion.yaml
