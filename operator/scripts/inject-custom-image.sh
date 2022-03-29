d5330f777cb84b3719837ee0ebfedfb151119cad
andreaTP <andrea.peruffo1982@gmail.com>
2022-03-29 15:41:38 +0100

    rem inject-custom-image

--- operator/scripts/inject-custom-image.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-set -euxo pipefail
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-VERSION="$1"
-VALUE="$2"
-
-yq ea -i ".spec.install.spec.deployments[0].spec.template.spec.containers[0].env += {\"name\": \"OPERATOR_KEYCLOAK_IMAGE\", \"value\": \"$VALUE\"}" $SCRIPT_DIR/../olm/$VERSION/manifests/keycloak-operator.v$VERSION.clusterserviceversion.yaml
