215b8a3f0420e46536f80673bfff87d18a2ce597
andreaTP <andrea.peruffo1982@gmail.com>
2022-04-12 10:03:47 +0100

    more tests

--- operator/scripts/prepare-olm-test.sh
+++ operator/scripts/prepare-olm-test.sh
@@ -14,17 +14,21 @@ OPERATOR_DOCKER_IMAGE="$DOCKER_REGISTRY/$OPERATOR_IMAGE_NAME"
 # TODO: clean this up
 # $SCRIPT_DIR/create-olm-bundle.sh $VERSION $PREV_VERSION $OPERATOR_DOCKER_IMAGE
 
-(cd $SCRIPT_DIR/../olm/$VERSION && \
-  docker build -t $DOCKER_REGISTRY/keycloak-operator-bundle:$VERSION -f bundle.Dockerfile . && \
-  docker push $DOCKER_REGISTRY/keycloak-operator-bundle:$VERSION)
+# (cd $SCRIPT_DIR/../olm/$VERSION && \
+#   docker build -t $DOCKER_REGISTRY/keycloak-operator-bundle:$VERSION -f bundle.Dockerfile . && \
+#   docker push $DOCKER_REGISTRY/keycloak-operator-bundle:$VERSION)
 
 # Verify the bundle
-opm alpha bundle validate --tag $DOCKER_REGISTRY/keycloak-operator-bundle:$VERSION --image-builder docker
+# ttl.sh
+# opm alpha bundle validate --tag $DOCKER_REGISTRY/keycloak-operator-bundle:$VERSION --image-builder docker
 
 # Create the test-catalog
 # Using `sudo` to make the command working on Mac:
 sudo opm index add --bundles $DOCKER_REGISTRY/keycloak-operator-bundle:$VERSION \
-  --tag $DOCKER_REGISTRY/keycloak-test-catalog:$VERSION -c docker
+  --tag $DOCKER_REGISTRY/keycloak-test-catalog:$VERSION -c docker --permissive
+
+# sudo opm index add --bundles 10.96.98.60/keycloak-operator-bundle:0.0.1-fc642f5   --tag=10.96.98.60/keycloak-test-catalog:0.0.1-fc642f5 -c docker -p docker
+
 docker push $DOCKER_REGISTRY/keycloak-test-catalog:$VERSION
 
 # Create testing resources
