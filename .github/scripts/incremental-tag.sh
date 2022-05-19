6c902521646c9646ac9363159e486f3779c7ec0f
andreaTP <andrea.peruffo1982@gmail.com>
2022-05-19 11:38:35 +0100

    incremental suffix

---
+++ .github/scripts/incremental-tag.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+set -euo pipefail
+
+image=$1
+tag=$2
+original_tag=$tag
+c=1
+
+while :
+do
+  tag="${original_tag}-${c}"
+  if DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect $image:$tag >/dev/null 2>/dev/null; then
+      c=$((c+1))
+  else
+      echo $tag
+      break
+  fi
+done
