ed8ef27b3b696c755f022d2f8283729e3f1e300e
andreaTP <andrea.peruffo1982@gmail.com>
2022-05-31 09:20:33 +0100

    health

--- config-api/src/main/java/org/keycloak/config/AllOptions.java
+++ config-api/src/main/java/org/keycloak/config/AllOptions.java
@@ -11,6 +11,7 @@ public class AllOptions {
         ALL_OPTIONS.addAll(ClusteringOptions.ALL_OPTIONS);
         ALL_OPTIONS.addAll(DatabaseOptions.ALL_OPTIONS);
         ALL_OPTIONS.addAll(FeatureOptions.ALL_OPTIONS);
+        ALL_OPTIONS.addAll(HealthOptions.ALL_OPTIONS);
         ALL_OPTIONS.addAll(HttpOptions.ALL_OPTIONS);
     }
 }
